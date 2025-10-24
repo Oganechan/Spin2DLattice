b8b57aed50e412b85305e0e227ec3f29df29f16d
Oganetyan <timofeev.nikita12@yandex.ru>
2025-10-24 10:25:06 +1000

    feat: add main.cpp as simulation entry point

---
+++ include/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "core/simulation.h"
+
+int main()
+{
+    Config config;
+    config.load("../data/input/default.json");
+
+    Simulation simulation(config, "../data/output");
+    simulation.run();
+
+    return 0;
+}
