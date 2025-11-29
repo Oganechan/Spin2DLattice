18f1fa7d8a122cd538b006a112547e415655fbbf
Oganechan <wantedman1205@gmail.com>
2025-11-30 01:49:46 +1000

    perf: use small spin rotations instead of full random spins for better convergence

--- include/algorithm/metropolis.cpp
+++ include/algorithm/metropolis.cpp
@@ -1,15 +1,14 @@
 #include "metropolis.hpp"
 
-algorithm::Metropolis::Metropolis(lattice::Atoms &atoms)
-    : atoms_(atoms), calculator_(atoms),
-      temperature_(atoms_.get_config().get<double>("physical.temperature")),
-      beta_(1.0 / temperature_) {
+algorithm::Metropolis::Metropolis(lattice::Atoms &atoms,
+                                  const physics::Calculator &calculator)
+    : atoms_(atoms), calculator_(calculator) {
     Random::uniform_real<double>();
 }
 
 void algorithm::Metropolis::step() {
     const int32_t atom_id = atoms_.select_random_magnetic_atom();
-    const std::array<double, 3> new_spin = atoms_.generate_random_spin();
+    const std::array<double, 3> new_spin = atoms_.small_rotate_spin(atom_id);
 
     const double energy_diff =
         calculator_.calculate_flip_energy_difference(atom_id, new_spin);
@@ -19,8 +18,19 @@ void algorithm::Metropolis::step() {
 }
 
 void algorithm::Metropolis::sweep() {
-    int32_t magnetic_count = atoms_.get_magnetic_count() * 10;
+    int32_t step_count = atoms_.get_magnetic_count();
 
-    while (--magnetic_count > 0)
+    while (step_count-- > 0)
         step();
+}
+
+bool algorithm::Metropolis::accept_change(double energy_difference) const {
+    if (energy_difference <= 0)
+        return true;
+
+    double random_value = Random::uniform_real<double>();
+    double threshold =
+        std::exp(-energy_difference / calculator_.get_temperature());
+
+    return random_value <= threshold;
 }
