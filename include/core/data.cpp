7ae877d20b167c67e747d0b1461c8b3333560ff2
Oganechan <wantedman1205@gmail.com>
2026-01-23 22:58:32 +1000

    changed the data to be saved to a file

--- include/core/data.cpp
+++ include/core/data.cpp
@@ -1,4 +1,5 @@
 #include "data.hpp"
+#include <cstdint>
 #include <fstream>
 
 Data::Data(const physics::Calculator &calculator,
@@ -8,39 +9,94 @@ Data::Data(const physics::Calculator &calculator,
 void Data::measure() {
     double energy = calculator_.calculate_total_energy();
     double magnetization = calculator_.calculate_total_magnetization();
+    double order_parameter = calculator_.calculate_total_order();
 
     energies_.push_back(energy);
     magnetizations_.push_back(magnetization);
+    order_parameters_.push_back(order_parameter);
 }
 
 void Data::save() {
     compute_statistics();
 
     std::ofstream file(output_path_, std::ios::app);
+
     file << calculator_.get_concentration() << "\t"
-         << calculator_.get_temperature() << "\t" << mean_energy_ << "\t"
-         << mean_magnetization_ << "\n";
+         << calculator_.get_temperature()
+         << "\t"
+
+         // EnergyI
+         << mean_energy_ << "\t" << specific_heat_
+         << "\t"
+
+         // Order parameter
+         << mean_order_parameter_ << "\t" << order_susceptibility_ << "\t"
+         << order_binder_ << "\n";
 }
 
 void Data::reset() {
     energies_.clear();
     magnetizations_.clear();
+    order_parameters_.clear();
 
     mean_energy_ = 0.0;
-    mean_magnetization_ = 0.0;
+    mean_energy_sq = 0.0;
+
+    mean_order_parameter_ = 0.0;
+    mean_order_parameter_abs_ = 0.0;
+    mean_order_parameter_sq_ = 0.0;
+    mean_order_parameter_4th_ = 0.0;
+
+    specific_heat_ = 0.0;
+    order_susceptibility_ = 0.0;
+    order_binder_ = 0.0;
 }
 
 void Data::compute_statistics() {
-    if (energies_.empty() || magnetizations_.empty())
+    if (energies_.empty() || magnetizations_.empty() ||
+        order_parameters_.empty())
         return;
 
-    double energy_sum = 0.0, magnetization_sum = 0.0;
+    int32_t N = energies_.size();
+    int32_t magnetic_count = calculator_.get_atoms().get_magnetic_count();
+    double temperature = calculator_.get_temperature();
 
-    for (int32_t i = 0; i < energies_.size(); ++i) {
-        energy_sum += energies_[i];
-        magnetization_sum += magnetizations_[i];
+    if (temperature < 1.0e-5)
+        return;
+
+    double energy_sum = 0.0, energy_sum_sq = 0.0;
+    double order_sum = 0.0, order_abs_sum = 0.0, order_sq_sum = 0.0,
+           order_4th_sum = 0.0;
+
+    for (int32_t i = 0; i < N; ++i) {
+        double e = energies_[i];
+        energy_sum += e;
+        energy_sum_sq += e * e;
+
+        double phi = order_parameters_[i];
+        double phi_abs = std::abs(phi);
+        order_sum += phi;
+        order_abs_sum += phi_abs;
+        order_sq_sum += phi * phi;
+        order_4th_sum += phi * phi * phi * phi;
     }
 
-    mean_energy_ = energy_sum / energies_.size();
-    mean_magnetization_ = magnetization_sum / magnetizations_.size();
+    mean_energy_ = energy_sum / N;
+    mean_energy_sq = energy_sum_sq / N;
+
+    mean_order_parameter_ = order_sum / N;
+    mean_order_parameter_abs_ = order_abs_sum / N;
+    mean_order_parameter_sq_ = order_sq_sum / N;
+    mean_order_parameter_4th_ = order_4th_sum / N;
+
+    specific_heat_ = (mean_energy_sq - mean_energy_ * mean_energy_) /
+                     (magnetic_count * temperature * temperature);
+
+    order_susceptibility_ = (mean_order_parameter_sq_ -
+                             mean_order_parameter_ * mean_order_parameter_) /
+                            (magnetic_count * temperature);
+
+    order_binder_ =
+        1.0 - mean_order_parameter_4th_ /
+                  (3.0 * mean_order_parameter_sq_ * mean_order_parameter_sq_);
 }
--- include/core/data.hpp
+++ include/core/data.hpp
@@ -3,28 +3,57 @@
 
 #include "../physics/calculator.hpp"
 #include <string>
+#include <vector>
 
 class Data {
   public:
-    Data(const physics::Calculator &calculator, const std::string &output_path);
+    explicit Data(const physics::Calculator &calculator,
+                  const std::string &output_path);
 
     void measure();
     void save();
     void reset();
 
     inline double get_mean_energy() const { return mean_energy_; }
-    inline double get_mean_magnetization() const { return mean_magnetization_; }
+    inline double get_mean_energy_sq() const { return mean_energy_sq; }
+
+    inline double get_mean_order_parameter() const {
+        return mean_order_parameter_;
+    }
+    inline double get_mean_order_parameter_sq() const {
+        return mean_order_parameter_sq_;
+    }
+    inline double get_mean_order_parameter_4th() const {
+        return mean_order_parameter_4th_;
+    }
+
+    inline double get_specific_heat() const { return specific_heat_; }
+
+    inline double get_order_susceptibility() const {
+        return order_susceptibility_;
+    }
+
+    inline double get_order_binder() const { return order_binder_; }
 
   private:
     const physics::Calculator &calculator_;
-
     const std::string &output_path_;
 
     std::vector<double> energies_;
     std::vector<double> magnetizations_;
+    std::vector<double> order_parameters_;
 
     double mean_energy_ = 0.0;
-    double mean_magnetization_ = 0.0;
+    double mean_energy_sq = 0.0;
+
+    double mean_order_parameter_ = 0.0;     // ⟨φ⟩
+    double mean_order_parameter_abs_ = 0.0; // ⟨|φ|⟩
+    double mean_order_parameter_sq_ = 0.0;  // ⟨φ²⟩
+    double mean_order_parameter_4th_ = 0.0; // ⟨φ⁴⟩
+
+    double specific_heat_ = 0.0;
+    double order_susceptibility_ = 0.0;
+    double order_binder_ = 0.0;
 
     void compute_statistics();
 };
